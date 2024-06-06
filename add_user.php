2b8c335aa78ce565b84e82bc0d874ec82195e390
Nasshameed <112845081+Nasshameed@users.noreply.github.com>
2024-06-06 08:27:24 +0300

    Add files via upload

---
+++ add_user.php
@@ -0,0 +1,5 @@
+<?php require_once "./assets/header.php";?>
+<?php require_once "./assets/sidebar.php";?>
+<?php require_once "./assets/navbar.php";?>
+<?php require_once "./assets/add_user.php";?>
+<?php require_once "./assets/footer.php";?>
